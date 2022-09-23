0ffc58a64f01ec9b462ce77e99c6ab011c98a30b
Rubén López Martínez <ruben.lopezm@estudiants.urv.cat>
2022-09-23 20:38:45 +0200

    prog3: afegit correu prog3 al main i move

--- source/candy1_move.s
+++ source/candy1_move.s
@@ -1,8 +1,8 @@
 @;=                                                         	      	=
 @;=== candy1_move: rutinas para contar repeticiones y bajar elementos ===
 @;=                                                          			=
-@;=== Programador tarea 1E: xxx.xxx@estudiants.urv.cat				  ===
-@;=== Programador tarea 1F: yyy.yyy@estudiants.urv.cat				  ===
+@;=== Programador tarea 1E: ruben.lopezm@estudiants.urv.cat				  ===
+@;=== Programador tarea 1F: ruben.lopezm@estudiants.urv.cat				  ===
 @;=                                                         	      	=
 
 
