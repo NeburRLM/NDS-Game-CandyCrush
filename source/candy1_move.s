3f00849fb3f95e445cddb0c58c62ad312093a962
Inés Ortiz <ines.ortiz@estudiants.urv.cat>
2022-09-30 08:31:10 +0200

    prog2: afegit fitxer candy1_move

---
+++ source/candy1_move.s
@@ -0,0 +1,177 @@
+@;=                                                         	      	=
+@;=== candy1_move: rutinas para contar repeticiones y bajar elementos ===
+@;=                                                          			=
+@;=== Programador tarea 1E: xxx.xxx@estudiants.urv.cat				  ===
+@;=== Programador tarea 1F: yyy.yyy@estudiants.urv.cat				  ===
+@;=                                                         	      	=
+
+
+
+.include "../include/candy1_incl.i"
+
+MASK_VALOR_SENSE_GEL = 0b00111
+
+
+@;-- .text. código de las rutinas ---
+.text	
+		.align 2
+		.arm
+
+
+
+@;TAREA 1E;
+@; cuenta_repeticiones(*matriz,f,c,ori): rutina para contar el número de
+@;	repeticiones del elemento situado en la posición (f,c) de la matriz, 
+@;	visitando las siguientes posiciones según indique el parámetro de
+@;	orientación 'ori'.
+@;	Restricciones:
+@;		* sólo se tendrán en cuenta los 3 bits de menor peso de los códigos
+@;			almacenados en las posiciones de la matriz, de modo que se ignorarán
+@;			las marcas de gelatina (+8, +16)
+@;		* la primera posición también se tiene en cuenta, de modo que el número
+@;			mínimo de repeticiones será 1, es decir, el propio elemento de la
+@;			posición inicial
+@;	Parámetros:
+@;		R0 = dirección base de la matriz
+@;		R1 = fila 'f'
+@;		R2 = columna 'c'
+@;		R3 = orientación 'ori' (0 -> Este, 1 -> Sur, 2 -> Oeste, 3 -> Norte)
+@;	Resultado:
+@;		R0 = número de repeticiones detectadas (mínimo 1)
+	.global cuenta_repeticiones
+cuenta_repeticiones:														@;		   |
+		push {r1-r12,lr}													@;oest<--|   |--->est
+																			@;         |
+																			@;		  sud
+		
+		mov r4, #1 									@;r4 contindrà num_repeticions, inicialment =1
+		mov r5, #COLUMNS 							@;columnas (carreguem com a valor inmediat ja que es menor que 32 bits)
+		mov r6, #ROWS 								@;filas (carreguem com a valor inmediat ja que es menor que 32 bits)
+			
+		mla r9, r1, r5, r2
+		ldrb r8, [r0,r9]
+		and r10, r8, #MASK_VALOR_SENSE_GEL			@;apliquem màscara per quedarnos amb els 3 bits de menor pes
+		cmp r3, #1 									@;analitzem la orientació
+		bgt .Nord_Oest 								@;si r3>1 salta (ja que podria ser 2->oest o 3->nord)
+		beq .Sud 									@;si r3==1, salta a Sud
+		
+		.Est: 										@;r3 no és ni 1 ni és més gran que 1, per tant 0 (Est)
+		sub r7, r5, #1 								@;r7=COLUMNS-1=8 (per a comparar dins de la posició màxima de la matriu)
+		cmp r2, r7 									@;comparem la columna actual amb el màxim de columnes de la matriu
+		bge .Fi 									@;si r2>=r7 (si la columna actual es superior o igual a la màxima), acaba el programa
+		add r2,#1									@;si segueix dins de les dimensions de la matriu, s'incrementa una columna per seguir amb el recorregut Est
+		mla r9,r1,r5,r2 							@;obtenim la nova posició segons la nova columna a estudiar
+		ldrb r8, [r0,r9]							@;obtenim el valor de la nova posició calculada
+		and r8, r8, #MASK_VALOR_SENSE_GEL			@;apliquem màscara per quedarnos amb els 3 bits de menor pes
+		cmp r8,r10									@;comparem el valor recent amb el inicial (aplicada ja la màscara)
+		bne .Fi										@;si son diferents, s'acaba el recorregut i el programa
+		add r4,#1									@;si els dos valors son iguals, s'incrementa el número de repeticions consecutius 
+		b .Est										@;torna a començar el bucle del recorregut
+		
+		.Sud: 										@;r3 és 1
+		sub r7, r6, #1 								@;r7=ROWS-1=8 (per a comparar dins de la posició màxima de la matriu)
+		cmp r1, r7 									@;comparem la fila actual amb el màxim de columnes de la matriu
+		bge .Fi 									@;si r1>=r7 (si la fila actual es superior o igual a la màxima), acaba el programa
+		add r1,#1									@;si segueix dins de les dimensions de la matriu, s'incrementa una fila per seguir amb el recorregut Sud
+		mla r9,r1,r5,r2 							@;obtenim la nova posició segons la nova fila a estudiar
+		ldrb r8, [r0,r9]							@;obtenim el valor de la nova posició calculada
+		and r8, r8, #MASK_VALOR_SENSE_GEL			@;apliquem màscara per quedarnos amb els 3 bits de menor pes
+		cmp r8,r10									@;comparem el valor recent amb el inicial (aplicada ja la màscara)
+		bne .Fi										@;si son diferents, s'acaba el recorregut i el programa
+		add r4,#1									@;si els dos valors son iguals, s'incrementa el número de repeticions consecutius
+		b .Sud										@;torna a començar el bucle del recorregut
+		
+		.Nord_Oest:									@;salt a estudiar possible nord o oest
+		cmp r3, #2 									@;comparem la orientació amb el valor 2
+		beq .Oest									@;si r3==2, té orientació de oest, sinò serà nord
+		
+		.Nord: 										@;si no es oest, serà nord
+		cmp r1, #0 									@;comparem amb la posició mínima de la matriu
+		ble .Fi										@;si r1<=0 (si la fila actual es inferior o igual a la mínima), acaba el programa
+		sub r1,#1									@;si segueix dins de les dimensions de la matriu, restem una fila per fer el recorregut NORD
+		mla r9,r1,r5,r2 							@;obtenim la nova posició segons la nova fila a estudiar
+		ldrb r8, [r0,r9]							@;obtenim el valor de la nova posició calculada
+		and r8, r8, #MASK_VALOR_SENSE_GEL			@;apliquem màscara per quedarnos amb els 3 bits de menor pes
+		cmp r8,r10									@;comparem el valor recent amb el inicial (aplicada ja la màscara)
+		bne .Fi										@;si son diferents, s'acaba el recorregut i el programa
+		add r4,#1									@;si els dos valors son iguals, s'incrementa el número de repeticions consecutius
+		b .Nord										@;torna a començar el bucle del recorregut
+			
+		.Oest:										@;r3 és 2 (oest)
+		cmp r2, #0 									@;comparem amb la posició mínima de la matriu
+		ble .Fi 									@;si r2<=0 (si la columna actual es inferior o igual a la mínima), acaba el programa
+		sub r2,#1 									@;si segueix dins de les dimensions de la matriu, restem una columna per fer el recorregut OEST
+		mla r9,r1,r5,r2 							@;obtenim la nova posició segons la nova columna a estudiar
+		ldrb r8, [r0,r9]							@;obtenim el valor de la nova posició calculada
+		and r8, r8, #MASK_VALOR_SENSE_GEL			@;apliquem màscara per quedarnos amb els 3 bits de menor pes
+		cmp r8,r10									@;comparem el valor recent amb el inicial (aplicada ja la màscara)
+		bne .Fi										@;si son diferents, s'acaba el recorregut i el programa
+		add r4,#1									@;si els dos valors son iguals, s'incrementa el número de repeticions consecutius
+		b .Oest										@;torna a començar el bucle del recorregut
+		
+			
+		.Fi:										@;acaba el programa
+		mov r0, r4									@;guardem el valor del registre r4 al registre r0
+		
+		pop {r1-r12, pc}
+
+
+
+@;TAREA 1F;
+@; baja_elementos(*matriz): rutina para bajar elementos hacia las posiciones
+@;	vacías, primero en vertical y después en sentido inclinado; cada llamada a
+@;	la función sólo baja elementos una posición y devuelve cierto (1) si se ha
+@;	realizado algún movimiento, o falso (0) si está todo quieto.
+@;	Restricciones:
+@;		* para las casillas vacías de la primera fila se generarán nuevos
+@;			elementos, invocando la rutina 'mod_random' (ver fichero
+@;			"candy1_init.s")
+@;	Parámetros:
+@;		R0 = dirección base de la matriz de juego
+@;	Resultado:
+@;		R0 = 1 indica se ha realizado algún movimiento, de modo que puede que
+@;				queden movimientos pendientes. 
+	.global baja_elementos
+baja_elementos:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+
+@;:::RUTINAS DE SOPORTE:::
+
+
+
+@; baja_verticales(mat): rutina para bajar elementos hacia las posiciones vacías
+@;	en vertical; cada llamada a la función sólo baja elementos una posición y
+@;	devuelve cierto (1) si se ha realizado algún movimiento.
+@;	Parámetros:
+@;		R4 = dirección base de la matriz de juego
+@;	Resultado:
+@;		R0 = 1 indica que se ha realizado algún movimiento. 
+baja_verticales:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+
+@; baja_laterales(mat): rutina para bajar elementos hacia las posiciones vacías
+@;	en diagonal; cada llamada a la función sólo baja elementos una posición y
+@;	devuelve cierto (1) si se ha realizado algún movimiento.
+@;	Parámetros:
+@;		R4 = dirección base de la matriz de juego
+@;	Resultado:
+@;		R0 = 1 indica que se ha realizado algún movimiento. 
+baja_laterales:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+
+.end
